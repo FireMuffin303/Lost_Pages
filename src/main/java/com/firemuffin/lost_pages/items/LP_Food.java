aa0ee44b880edca12f9804e7e24e6e1c3b930d82
FireMuffin303 <61814620+FireMuffin303@users.noreply.github.com>
2020-08-10 22:31:00 +0700

    Bruh

---
+++ src/main/java/com/firemuffin/lost_pages/items/LP_Food.java
@@ -0,0 +1,10 @@
+package com.firemuffin.lost_pages.items;
+
+import com.firemuffin.lost_pages.Lostpages;
+import net.minecraft.item.Item;
+
+public class LP_Food extends Item {
+    public LP_Food() {
+        super(new Item.Properties().group(Lostpages.FOOD));
+    }
+}
---
+++ src/main/java/com/firemuffin/lost_pages/items/LP_Meterial.java
@@ -0,0 +1,13 @@
+package com.firemuffin.lost_pages.items;
+
+import com.firemuffin.lost_pages.Lostpages;
+import net.minecraft.item.Item;
+
+public class LP_Meterial extends Item {
+    public LP_Meterial() {
+        super(new Item.Properties().group(Lostpages.METERIAL));
+    }
+}
+
+
+
